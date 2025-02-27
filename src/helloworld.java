f6e2e48e602edd50c624819cdd3ef89d9ad1c76e
Maheshrathinasamy5 <maheshrathinasamy5@gmail.com>
2025-02-27 17:29:57 +0530

    hello world

--- src/helloworld.java
+++ src/helloworld.java
@@ -1,8 +1,5 @@
-import java.util.Scanner;
-
 public class helloworld {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
         System.out.println("Hello World");
     }
 }
